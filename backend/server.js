f87d7dee2bfaa385e598181227218b7e48a8bb07
abdussameea1813 <abdussameeapatel18@gmail.com>
2025-02-03 14:47:06 -0500

    3. Backend Done

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,20 @@
 import express from "express"; 
-import authRoutes from "./routes/auth.routes.js"
+import authRoutes from "./routes/auth.route.js"
+import userRoutes from "./routes/users.route.js"
+import postRoutes from "./routes/post.route.js"
+import notificationRoutes from "./routes/notification.route.js";
 import dotenv from "dotenv"
 import connectMongoDB from "./db/connectMongoDB.js";
 import cookieParser from "cookie-parser";
+import { v2 as cloudinary } from "cloudinary";
 
 dotenv.config();
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARU_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 const app = express();
 
@@ -16,10 +26,10 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(cookieParser());
 
-
-
-
-app.use("/api/auth", authRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
+app.use("/api/notifications", notificationRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
